3ddaf4279cc4c6d838235b2b1842f9cc423bf422
fredvigsoe <fredvigsoe@gmail.com>
2022-11-18 10:06:37 +0100

    ..

--- database.js
+++ database.js
@@ -10,10 +10,7 @@ const mysqlConnection = mysql.createConnection({
     multipleStatements: true,
 });
 
-console.log(process.env.HOST)
-console.log(process.env.PORT)
-console.log(process.env.MYSQL_USER)
-console.log(process.env.DATABASE)
+
 
 mysqlConnection.connect((err) => {
     if (!err) {
